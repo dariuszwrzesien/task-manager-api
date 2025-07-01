a151fdd8b27349d01e4c67680baa6b236fc713c5
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2025-07-01 17:13:05 +0200

    Reorder app initialization and port retrieval for improved clarity

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,18 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
-  const port = app
-    .get<ConfigService>(ConfigService)
-    .get<number>('APP_PORT', 3000);
 
+  const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS for all routes not good for production
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  const port = app
+    .get<ConfigService>(ConfigService)
+    .get<number>('APP_PORT', 3000);
   await app.listen(port);
+
   logger.log(`Application is running on port: ${port}`);
 }
 bootstrap();
