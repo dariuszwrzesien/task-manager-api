4539a8a07064bf43b9f0f2b8830e54c02ae73ba5
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2025-07-01 17:30:27 +0200

    Refactor database connection logic to conditionally enable SSL and synchronize based on environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,19 +16,31 @@ import { configValidationSchema } from './config.schema';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-        autoLoadEntities: true,
-        // Setting `synchronize: true` shouldn't be used in production - otherwise you can lose production data.
-        // Source: https://docs.nestjs.com/techniques/database
-        synchronize: true,
-        entities: [User, Task],
-      }),
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+        return {
+          ssl: isProduction,
+          extra: isProduction
+            ? {
+                ssl: {
+                  rejectUnauthorized: false,
+                },
+              }
+            : {},
+          // Database connection options
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          // Setting `synchronize: true` shouldn't be used in production - otherwise you can lose production data.
+          // Source: https://docs.nestjs.com/techniques/database
+          synchronize: isProduction ? false : true,
+          entities: [User, Task],
+        };
+      },
     }),
     AuthModule,
     TasksModule,
