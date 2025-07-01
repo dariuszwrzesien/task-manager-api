233cc396063c98fb9a2c0eb08a863765e5f1074d
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2025-07-01 17:08:51 +0200

    Refactor configuration and bootstrap logic to utilize APP_PORT from config schema

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const logger = new Logger('Bootstrap');
+  const port: number =
+    app.get<ConfigService>(ConfigService).get('APP_PORT') || 3000;
+
   app.enableCors(); // Enable CORS for all routes not good for production
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
